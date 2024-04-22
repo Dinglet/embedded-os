2ce50933cc98e971772b891c94175709babebef4
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2024-04-22 16:25:00 +0800

    Add simple socket program by referencing bind(2) in the system calls manual to send a hello-world message to a client

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
+
+int serverSocket = -1;
+int clientSocket = -1;
+
+void sigint_handler(int signum)
+{
+    if (serverSocket != -1)
+        close(serverSocket);
+}
 
 void handler(int signum)
 {
@@ -10,5 +23,59 @@ void handler(int signum)
 
 int main(int argc, char *argv[])
 {
+    struct sockaddr_in serverAddress, clientAddress;
+    socklen_t clientAddressSize = sizeof(clientAddress);
+
+    signal(SIGINT, sigint_handler);
     signal(SIGCHLD, handler);
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1)
+    {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddress.sin_port = htons(4444);
+
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+    {
+        perror("bind()");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(serverSocket, 5) == -1)
+    {
+        perror("listen()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Now we can accept incoming connections one
+        at a time using accept(2). */
+
+    clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientAddressSize);
+    if (clientSocket == -1)
+    {
+        perror("accept()");
+        exit(EXIT_FAILURE);
+    }
+
+    send(clientSocket, "Hello, World!", 13, 0);
+
+    if (close(clientSocket) == -1)
+    {
+        perror("close()");
+        exit(EXIT_FAILURE);
+    }
+
+    if (close(serverSocket) == -1)
+    {
+        perror("close()");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
 }
