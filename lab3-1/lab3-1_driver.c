31c87ed01ea681902b73ff48bdb6122b01f18e9a
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2024-06-08 14:58:11 +0800

    Update the author name and license information of the driver in lab 3-1

--- lab3-1/lab3-1_driver.c
+++ lab3-1/lab3-1_driver.c
@@ -1,13 +1,17 @@
-/***************************************************************************//**
-*  \file       driver.c
-*
-*  \details    Simple GPIO driver explanation
-*
-*  \author     EmbeTronicX
-*
-*  \Tested with Linux raspberrypi 5.4.51-v7l+
-*
-*******************************************************************************/
+/**
+ * @file lab3-1_driver.c
+ *
+ * @brief A device driver - GPIO Driver
+ *
+ * @details This driver controls 4 LEDs connected to GPIO pins.
+ *   Tested with Raspberry Pi Linux 6.1.77-rt24-v8+
+ *
+ * @author EmbeTronicX
+ *
+ * @author HUANG, SHIEN-TING
+ *
+ * @copyright GNU Public License
+ */
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
@@ -34,25 +38,25 @@ static struct gpio leds_gpios[] =
   {LED_2, GPIOF_OUT_INIT_LOW, "LED_2"},
   {LED_3, GPIOF_OUT_INIT_LOW, "LED_3"}
 };
- 
+
 dev_t dev = 0;
 static struct class *dev_class;
 static struct cdev etx_cdev;
- 
+
 static int __init etx_driver_init(void);
 static void __exit etx_driver_exit(void);
- 
- 
+
+
 /*************** Driver functions **********************/
 static int etx_open(struct inode *inode, struct file *file);
 static int etx_release(struct inode *inode, struct file *file);
-static ssize_t etx_read(struct file *filp, 
+static ssize_t etx_read(struct file *filp,
                 char __user *buf, size_t len,loff_t * off);
-static ssize_t etx_write(struct file *filp, 
+static ssize_t etx_write(struct file *filp,
                 const char *buf, size_t len, loff_t * off);
 /******************************************************/
- 
-//File operation structure 
+
+//File operation structure
 static struct file_operations fops =
 {
   .owner          = THIS_MODULE,
@@ -64,7 +68,7 @@ static struct file_operations fops =
 
 /*
 ** This function will be called when we open the Device file
-*/ 
+*/
 static int etx_open(struct inode *inode, struct file *file)
 {
   pr_info("Device File Opened...!!!\n");
@@ -82,14 +86,14 @@ static int etx_release(struct inode *inode, struct file *file)
 
 /*
 ** This function will be called when we read the Device file
-*/ 
-static ssize_t etx_read(struct file *filp, 
+*/
+static ssize_t etx_read(struct file *filp,
                 char __user *buf, size_t len, loff_t *off)
 {
   char msg[2] = "0\0";
   uint8_t gpio_state = 0;
   uint8_t n = 0;
-  
+
   // https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html#read-and-write
   len = min(2 - *off, len);
   if (len <= 0)
@@ -104,7 +108,7 @@ static ssize_t etx_read(struct file *filp,
       return -EFAULT;
     return 1;
   }
-  
+
   //reading GPIO values
   gpio_state = gpio_get_value(LED_0);
   pr_info("Read function : LED_0 = %d \n", gpio_state);
@@ -113,7 +117,7 @@ static ssize_t etx_read(struct file *filp,
   gpio_state = gpio_get_value(LED_1);
   pr_info("Read function : LED_1 = %d \n", gpio_state);
   n |= gpio_state << 1;
-  
+
   gpio_state = gpio_get_value(LED_2);
   pr_info("Read function : LED_2 = %d \n", gpio_state);
   n |= gpio_state << 2;
@@ -121,21 +125,21 @@ static ssize_t etx_read(struct file *filp,
   gpio_state = gpio_get_value(LED_3);
   pr_info("Read function : LED_3 = %d \n", gpio_state);
   n |= gpio_state << 3;
-  
+
   //write to user
   msg[0] = n + '0'; // convert to ASCII
   if ( copy_to_user(buf, msg, len) ) {
     return -EFAULT;
   }
-  
+
   *off += len;
   return len;
 }
 
 /*
 ** This function will be called when we write the Device file
-*/ 
-static ssize_t etx_write(struct file *filp, 
+*/
+static ssize_t etx_write(struct file *filp,
                 const char __user *buf, size_t len, loff_t *off)
 {
   uint8_t c = 0;
@@ -151,7 +155,7 @@ static ssize_t etx_write(struct file *filp,
   }
 
   n = isdigit(c) ? c - '0' : 0;
-  
+
   // write to GPIOs
   gpio_set_value(LED_0, n&1 ? 1 : 0);
   pr_info("Write Function : LED_0 Set = %d\n", n&1 ? 1 : 0);
@@ -161,14 +165,14 @@ static ssize_t etx_write(struct file *filp,
   pr_info("Write Function : LED_2 Set = %d\n", n&4 ? 1 : 0);
   gpio_set_value(LED_3, n&8 ? 1 : 0);
   pr_info("Write Function : LED_3 Set = %d\n", n&8 ? 1 : 0);
-  
+
   *off += len;
   return len;
 }
 
 /*
 ** Module Init function
-*/ 
+*/
 static int __init etx_driver_init(void)
 {
   // Allocate a range of char device numbers with base minor number, count, and name.
@@ -177,28 +181,28 @@ static int __init etx_driver_init(void)
     goto r_unreg;
   }
   pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
- 
+
   /*Creating cdev structure*/
   cdev_init(&etx_cdev,&fops);
- 
+
   /*Adding character device to the system*/
   if((cdev_add(&etx_cdev,dev,1)) < 0){
     pr_err("Cannot add the device to the system\n");
     goto r_del;
   }
- 
+
   /*Creating struct class*/
   if(IS_ERR(dev_class = class_create(THIS_MODULE,"etx_class"))){
     pr_err("Cannot create the struct class\n");
     goto r_class;
   }
- 
+
   /*Creating device*/
   if(IS_ERR(device_create(dev_class,NULL,dev,NULL,"etx_device"))){
     pr_err( "Cannot create the Device \n");
     goto r_device;
   }
-  
+
   //Checking the GPIO is valid or not
   if (gpio_is_valid(LED_0) == false)
   {
@@ -220,27 +224,27 @@ static int __init etx_driver_init(void)
     pr_err("GPIO %d is not valid\n", LED_3);
     goto r_device;
   }
-  
+
   //Requesting and configuring multiple GPIOs in a single call
   if (gpio_request_array(leds_gpios, ARRAY_SIZE(leds_gpios)) < 0)
   {
     pr_err("Unable to request GPIOs\n");
     goto r_gpio;
   }
-  
+
   /* Using this call the GPIO 21 will be visible in /sys/class/gpio/
   ** Now you can change the gpio values by using below commands also.
   ** echo 1 > /sys/class/gpio/gpio21/value  (turn ON the LED)
   ** echo 0 > /sys/class/gpio/gpio21/value  (turn OFF the LED)
   ** cat /sys/class/gpio/gpio21/value  (read the value LED)
-  ** 
+  **
   ** the second argument prevents the direction from being changed.
   */
   // gpio_export(GPIO_21, false);
-  
+
   pr_info("Device Driver Insert...Done!!!\n");
   return 0;
- 
+
 r_gpio:
   gpio_free_array(leds_gpios, ARRAY_SIZE(leds_gpios));
 r_device:
@@ -251,13 +255,13 @@ r_del:
   cdev_del(&etx_cdev);
 r_unreg:
   unregister_chrdev_region(dev,1);
-  
+
   return -1;
 }
 
 /*
 ** Module exit function
-*/ 
+*/
 static void __exit etx_driver_exit(void)
 {
   // gpio_unexport(GPIO_21);
@@ -268,11 +272,12 @@ static void __exit etx_driver_exit(void)
   unregister_chrdev_region(dev, 1);
   pr_info("Device Driver Remove...Done!!\n");
 }
- 
+
 module_init(etx_driver_init);
 module_exit(etx_driver_exit);
- 
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("EmbeTronicX <embetronicx@gmail.com>");
-MODULE_DESCRIPTION("A simple device driver - GPIO Driver");
-MODULE_VERSION("1.32");
+MODULE_AUTHOR("HUANG, SHIEN-TING <10545636+Dinglet@users.noreply.github.com>");
+MODULE_DESCRIPTION("A device driver - GPIO Driver");
+MODULE_VERSION("1.00");
